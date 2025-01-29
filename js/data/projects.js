7cc6b3b84be1646cab07b63edcfbc642633abd73
iker-munoz-molero <iker.munoz.webdev@gmail.com>
2025-01-29 22:19:06 +0100

    Cleared duped projects

--- js/data/projects.js
+++ js/data/projects.js
@@ -4,24 +4,4 @@ const projects = [
         image: "images/websites/roman_larrodes.webp",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat."
     },
-    {
-        name: "Román Larrodé",
-        image: "images/websites/roman_larrodes.webp",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat."
-    },
-    {
-        name: "Román Larrodé",
-        image: "images/websites/roman_larrodes.webp",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat."
-    },
-    {
-        name: "Román Larrodé",
-        image: "images/websites/roman_larrodes.webp",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat."
-    },
-    {
-        name: "Román Larrodé",
-        image: "images/websites/roman_larrodes.webp",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea  commodo consequat."
-    },
 ]
