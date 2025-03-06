3bd0a62008af9ad4f37fd785f95ba035e41e2234
Panik <munoz.molero.iker@gmail.com>
2025-03-06 17:35:13 +0100

    Added Padis redesign project

--- js/data/projects.js
+++ js/data/projects.js
@@ -4,4 +4,9 @@ const projects = [
         image: "images/websites/roman_larrodes.webp",
         description: "Designing this website has been a challenge, not so much in terms of visuals, but in capturing the essence of Roman—an exceptional photographer—as he truly deserves, and reflecting his style in a way that does him justice."
     },
+    {
+        name: "Padis",
+        image: "images/websites/padis.webp",
+        description: "Designing the website for Padis was all about bringing the excitement of trading cards to life in a digital space. It wasn’t just about designing a storefront—it was about capturing the thrill of discovery, the nostalgia of collecting, and the energy of a community that shares a love for the game. Every element was crafted to reflect that passion, making the experience as immersive and engaging as stepping into the shop itself."
+    }
 ]
